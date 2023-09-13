b1e873ad25e248e343e741d9ea41fe9d87aff6f0
Maksim <maksim.bobrov@nuacom.net>
2023-09-13 14:59:38 +0300

    project added

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+AOS.init();
+
+$(document).ready(function () {
+    $('#portfolio_slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    const dots = '.slick-dots'
+    $('.slick-prev').addClass('prev')
+    $('.slick-next').addClass('next')
+    $(dots).attr('data-aos', 'fade-up')
+    $(dots).addClass('portfolio_pages')
+    $('.slick-list').addClass('portfolio_box')
+    $('#slick-slide-control00').text('react / jquery / javascript')
+    $('#slick-slide-control01').text('tailwind / bootstrap')
+    $('#slick-slide-control02').text('cucumber / playwright / cypress')
+
+
+    $('#testimonials_slider').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.mobile_nav_toggle').click(function () {
+        $('#navbar').toggleClass('d-none');
+    });
+
+    $('#nav_menu a').click(function () {
+        $('#navbar').addClass('d-none')
+    })
+})
